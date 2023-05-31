c7750b48bd36b8a89a0acc9f4c809d99cf5dcf14
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-05-31 14:15:42 +0600

    meta icon added

--- frontend/src/components/HomePage/Navbar/index.tsx
+++ frontend/src/components/HomePage/Navbar/index.tsx
@@ -15,7 +15,7 @@ function Navbar() {
       <header className="text-gray-600 body-font bg-white">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               stroke="currentColor"
@@ -26,7 +26,7 @@ function Navbar() {
               viewBox="0 0 24 24"
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
+            </svg> */}
             <span className="ml-3 text-xl">Tailblocks</span>
           </a>
           {/* search start*/}
