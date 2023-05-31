38c6cf92b97d00930fb6852a3bc89ded7b6ba4fe
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-05-31 16:21:48 +0600

    Modified Explore Menu in Navbar

--- frontend/src/components/HomePage/Navbar/Explore.tsx
+++ frontend/src/components/HomePage/Navbar/Explore.tsx
@@ -43,24 +43,6 @@ export default function ExploreMenu(props: ExploreInterface) {
             overflow: "visible",
             filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            "&:before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
           },
         }}
         transformOrigin={{ horizontal: "right", vertical: "top" }}
