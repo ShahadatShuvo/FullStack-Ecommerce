1674f777409440f90ad89d6acbfee6c5879a9b1a
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-01 19:20:51 +0600

    working on product card

--- frontend/src/components/HomePage/NewArrival/ProductCard.tsx
+++ frontend/src/components/HomePage/NewArrival/ProductCard.tsx
@@ -10,11 +10,24 @@ import { Button } from "@mui/material";
 
 function ProductCard() {
   const [favourite, setFavourite] = React.useState(false);
+  const [view, setView] = React.useState(false);
+
   const onHandleFavourite = () => {
     setFavourite((prevState) => !prevState);
   };
+  const onHandleViewOpen = () => {
+    setView(true);
+  };
+  const onHandleViewClose = () => {
+    setView(false);
+  };
+
   return (
-    <div className="card relative">
+    <div
+      className="card relative"
+      onMouseEnter={onHandleViewOpen}
+      onMouseLeave={onHandleViewClose}
+    >
       <div className="absolute right-2 top-1 p-2 bg-white rounded-full">
         {!favourite && (
           <FavoriteBorderRoundedIcon onClick={onHandleFavourite} />
@@ -26,16 +39,18 @@ function ProductCard() {
           />
         )}
       </div>
-      <div className="absolute bottom-4 w-full">
-        <div className="px-2 flex justify-around">
-          <Button className="text-xs font-semibold py-1 rounded-full bg-black hover:bg-gray-600 text-white flex justify-center items-center drop-shadow-md hover:drop-shadow-xl">
-            <NextWeekOutlinedIcon />
-            <span className="ml-1">Add to bag</span>
-          </Button>
+      {view && (
+        <div className="absolute bottom-4 w-full">
+          <div className="px-2 flex justify-around">
+            <Button className="text-xs font-semibold py-1 rounded-full bg-black hover:bg-gray-600 text-white flex justify-center items-center drop-shadow-md hover:drop-shadow-xl">
+              <NextWeekOutlinedIcon />
+              <span className="ml-1">Add to bag</span>
+            </Button>
 
-          <CartViewDialogue />
+            <CartViewDialogue />
+          </div>
         </div>
-      </div>
+      )}
       <div className="p-5 bg-blue-50 rounded-lg flex justify-center items-center">
         <Image
           src="/img/cart/sshoe.png"
--- frontend/src/components/HomePage/NewArrival/index.tsx
+++ frontend/src/components/HomePage/NewArrival/index.tsx
@@ -8,7 +8,7 @@ function NewArrival() {
     <div className="my-5 mx-16">
       <h2 className=" text-4xl font-semibold">
         New Arrivals.
-        <span className="text-gray-500">REY backpacks & bags</span>
+        <span className="text-gray-500 ml-2">REY backpacks & bags</span>
       </h2>
       <div className="my-12 flex justify-between">
         <ProductCard />
