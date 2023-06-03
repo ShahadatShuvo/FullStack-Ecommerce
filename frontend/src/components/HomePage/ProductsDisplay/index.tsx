39597bb9db9c948a2c777fe9e453b76f35f94a21
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-03 16:13:48 +0600

    working with api fetch

--- frontend/src/components/HomePage/ProductsDisplay/index.tsx
+++ frontend/src/components/HomePage/ProductsDisplay/index.tsx
@@ -2,8 +2,41 @@
 
 import { Pagination } from "@mui/material";
 import ProductCard from "../NewArrival/ProductCard";
+import { useEffect, useState } from "react";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const USERNAME = "shahadat"; // Replace with your username
+const PASSWORD = "bangladesh7860"; // Replace with your password
 
 function ProductDisplay() {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null || "Error");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`, {
+          headers: {
+            Authorization: `Basic ${btoa(`${USERNAME}:${PASSWORD}`)}`, // Base64 encoded username:password
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setData(result);
+        } else {
+          throw new Error("Request failed");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log("data: ", data);
+
   return (
     <div>
       <div className="grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5  mx-16">
