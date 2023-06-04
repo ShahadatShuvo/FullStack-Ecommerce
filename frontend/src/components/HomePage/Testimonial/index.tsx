a723ae033fa00fbcdc5cc6eb2c9c41b10cdb0225
mdmahin.pro <mdmahin.pro@gmail.com>
2023-06-04 20:24:31 +0600

    Testimonial Added

--- frontend/src/components/HomePage/Testimonial/index.tsx
+++ frontend/src/components/HomePage/Testimonial/index.tsx
@@ -59,20 +59,11 @@ const Testimonial = () => {
         <h1 className="text-7xl text-center">Testimonial</h1>
       </div>
       <div className=" flex justify-center item">
-        <div className="flex flex-col items-center justify-center  w-2/3 ">
+        <div className="flex flex-col items-center justify-center w-[50vw] ">
           <div className="shadow-lg border col-span-2 border-gray-500 rounded">
             {/* <!-- Magic Dots --> */}
             <div className="relative flex p-4 justify-between  border-b border-gray-500">
               <h3 className="text-lg font-bold flex justify-center items-center">
-                {/* <svg
-                  className="MuiSvgIcon-root MuiSvgIcon-fontSizeLarge css-tzssek-MuiSvgIcon-root"
-                  focusable="false"
-                  aria-hidden="true"
-                  viewBox="0 0 24 24"
-                  data-testid="AccountCircleIcon"
-                >
-                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88C7.55 15.8 9.68 15 12 15s4.45.8 6.14 2.12C16.43 19.18 14.03 20 12 20z"></path>
-                </svg> */}
                 <img
                   className="h-16 w-16 rounded-full"
                   src="https://www.clipartmax.com/png/small/17-172602_computer-icons-user-profile-male-portrait-of-a-man.png"
