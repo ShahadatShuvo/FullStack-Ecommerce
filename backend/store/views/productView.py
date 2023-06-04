dc0241fe86fb36d1e3734799cd151c07051313e5
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-04 21:40:51 +0600

    working on Search functionality

--- backend/store/views/productView.py
+++ backend/store/views/productView.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.authentication import BasicAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 # Create your views here.
@@ -11,8 +12,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['id', 'price', 'stock', 'created_at', 'updated_at']
+    search_fields = ['title', 'price', 'description']
 
     # authentication_classes = [BasicAuthentication]
     # permission_classes = [permissions.IsAuthenticated]
