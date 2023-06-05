0089a34db97d47f8e53dabe8b7ab7034df4cec84
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-05 15:12:48 +0600

    working on pagination

--- backend/myEcom/settings/main.py
+++ backend/myEcom/settings/main.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.IsAuthenticated',
     # ]
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 12,
 }
 
 
