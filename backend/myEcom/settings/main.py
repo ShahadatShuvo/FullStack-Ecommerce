c563136c0ba3240ed9eebfd263146404bb72ed4f
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-05 15:00:58 +0600

    working on pagination backend

--- backend/myEcom/settings/main.py
+++ backend/myEcom/settings/main.py
@@ -123,15 +123,17 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 MEDIA_URL = '/images/'
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework.authentication.BasicAuthentication',
-#         # 'rest_framework.authentication.SessionAuthentication',
-#     ],
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.IsAuthenticated',
-#     ]
-# }
+REST_FRAMEWORK = {
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.BasicAuthentication',
+    #     # 'rest_framework.authentication.SessionAuthentication',
+    # ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ]
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
+}
 
 
 CORS_ORIGIN_ALLOW_ALL = True  # new
