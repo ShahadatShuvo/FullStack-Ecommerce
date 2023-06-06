93640d242e58e753c3275177331054f1809f27af
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-06 15:55:35 +0600

    solving problem

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path("api/products/", views.ProductList.as_view()),
     path("api/product/<str:pk>/", views.ProductDetail.as_view()),
 
-    # path("api/categories/", views.CategoryList.as_view()),
-
     path("api/customers/", views.CustomerList.as_view()),
     path("api/orders/", views.OrderList.as_view()),
 
