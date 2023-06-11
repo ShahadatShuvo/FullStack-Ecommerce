18f0fbc46e8e57d7fbf9331e845e55ed62e00672
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-11 21:44:25 +0600

    refactored code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 import HomePage from "@/components/HomePage";
 import AllPages from "./_page";
+import Navbar from "@/components/HomePage/Navbar";
+import Footer from "@/components/HomePage/Footer";
 
 export default function Home() {
   return (
