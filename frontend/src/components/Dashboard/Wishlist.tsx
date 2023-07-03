e1fbe712e30c0048c77b811d7e2e1ef1513570c7
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-07-03 14:05:43 +0600

    refactored code

--- frontend/src/components/Dashboard/Wishlist.tsx
+++ frontend/src/components/Dashboard/Wishlist.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ProductCard from "../ProductCard";
 import { demoProductCard } from "../../../interfaces";
 
-function Favourites() {
+function Wishlist() {
   return (
     <div className="py-24">
       <div className="flex flex-wrap justify-center items-center gap-5 px-16">
@@ -18,4 +18,4 @@ function Favourites() {
   );
 }
 
-export default Favourites;
+export default Wishlist;
