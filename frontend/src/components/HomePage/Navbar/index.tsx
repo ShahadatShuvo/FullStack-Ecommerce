6668cd78f678b42c98965f49ae2488267386c8d7
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-07-07 05:23:48 +0600

    dark mode toggle button UI changed

--- frontend/src/components/HomePage/Navbar/index.tsx
+++ frontend/src/components/HomePage/Navbar/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { GlobalStates } from "@/app/context";
 import CloseIcon from "@mui/icons-material/Close";
 import FeedOutlinedIcon from "@mui/icons-material/FeedOutlined";
@@ -9,66 +10,14 @@ import Alert from "@mui/material/Alert";
 import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
 import Image from "next/image";
-import { styled } from "@mui/material/styles";
 import { useContext, useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
 import CartMenu from "./CartMenu";
 import ExploreMenu from "./Explore";
 import MenuBarIcon from "./ProfileMenu";
 import AuthSuccess from "@/components/Accounts/AuthSuccess";
-import Switch, { SwitchProps } from "@mui/material/Switch";
 import Link from "next/link";
-
-const IOSSwitch = styled((props: SwitchProps) => (
-  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-))(({ theme }) => ({
-  width: 42,
-  height: 26,
-  padding: 0,
-  "& .MuiSwitch-switchBase": {
-    padding: 0,
-    margin: 2,
-    transitionDuration: "300ms",
-    "&.Mui-checked": {
-      transform: "translateX(16px)",
-      color: "#fff",
-      "& + .MuiSwitch-track": {
-        backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "#65C466",
-        opacity: 1,
-        border: 0,
-      },
-      "&.Mui-disabled + .MuiSwitch-track": {
-        opacity: 0.5,
-      },
-    },
-    "&.Mui-focusVisible .MuiSwitch-thumb": {
-      color: "#33cf4d",
-      border: "6px solid #fff",
-    },
-    "&.Mui-disabled .MuiSwitch-thumb": {
-      color:
-        theme.palette.mode === "light"
-          ? theme.palette.grey[100]
-          : theme.palette.grey[600],
-    },
-    "&.Mui-disabled + .MuiSwitch-track": {
-      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
-    },
-  },
-  "& .MuiSwitch-thumb": {
-    boxSizing: "border-box",
-    width: 22,
-    height: 22,
-  },
-  "& .MuiSwitch-track": {
-    borderRadius: 26 / 2,
-    backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
-    opacity: 1,
-    transition: theme.transitions.create(["background-color"], {
-      duration: 500,
-    }),
-  },
-}));
+import "./toggleBtn.css";
 
 function Navbar() {
   const router = useRouter();
@@ -295,12 +244,20 @@ function Navbar() {
                 Login
               </Button>
             )}
-            <div className="ml-2">
-              <IOSSwitch
-                sx={{ m: 1 }}
+            <div className="ml-3">
+              <button
+                type="button"
+                className={
+                  isDarkTheme
+                    ? "btn btn-sm btn-toggle active"
+                    : "btn btn-sm btn-toggle"
+                }
+                data-toggle="button"
+                aria-pressed="false"
                 onClick={toggleTheme}
-                checked={isDarkTheme}
-              />
+              >
+                <div className="handle"></div>
+              </button>
             </div>
             <Badge
               badgeContent={cartData.length}
