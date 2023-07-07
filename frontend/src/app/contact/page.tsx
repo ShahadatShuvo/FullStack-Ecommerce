cf0967e98e8b314bdd1a58cfbc105e801ee3211e
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-07-07 15:56:11 +0600

    created contact page

---
+++ frontend/src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <section></section>
+      <section></section>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ frontend/src/components/contact/DeveloperProfile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DeveloperProfile() {
+  return <div>DeveloperProfile</div>;
+}
+
+export default DeveloperProfile;
