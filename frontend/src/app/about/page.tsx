3a67c1b5789e6f38cb0e00de8e68359068777f1a
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-07-08 02:14:58 +0600

    refactored code

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -7,9 +7,9 @@ function About() {
       id: 1,
       name: "Shahadat Hossain",
       jobTitle: "Frontend Developer",
-      portfolio: "shahadat.website",
+      portfolio: "https://shahadat-shuvo.me",
       emailLink: "mdmahin.pro@gmail.com",
-      imgUrl: "/img/mukto.jpg",
+      imgUrl: "/img/shahadat.jpg",
       about:
         "I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.",
       interests:
@@ -21,7 +21,7 @@ function About() {
       jobTitle: "Frontend Developer",
       portfolio: "www.mdmahin.netlify.app",
       emailLink: "mdmahin.pro@gmail.com",
-      imgUrl: "/img/shahadat.jpg",
+      imgUrl: "/img/mukto.jpg",
       about:
         "I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.",
       interests:
@@ -33,7 +33,7 @@ function About() {
       <div className="container">
         <MagicLine title="About Us" />
       </div>
-      <div className="w-screen min-h-[80vh] flex justify-center  gap-5 py-24">
+      <div className="w-screen min-h-[80vh] flex justify-center  gap-5 py-16">
         {devs.map((dev) => (
           <DeveloperProfile developerData={dev} key={dev.id} />
         ))}
