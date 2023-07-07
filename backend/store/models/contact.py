5cf15638f66813951a1ef2475525c91cb2a6519c
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-07-07 16:08:58 +0600

    created contact model

--- backend/store/models/contact.py
+++ backend/store/models/contact.py
@@ -1,12 +1,13 @@
-# from django.db import models
+from django.db import models
 
 
-# class Contact(models.Model):
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField(max_length=100)
-#     phone = models.CharField(max_length=100)
-#     subject = models.CharField(max_length=100)
-#     message = models.TextField(null=True, blank=True)
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    subject = models.CharField(max_length=255)
+    message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
