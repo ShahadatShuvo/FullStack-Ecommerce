2798c4dbc0382635b97a6a951cb08f9673706e28
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-07-07 18:25:32 +0600

    page not found added

--- frontend/src/app/error/404/page.tsx
+++ frontend/src/app/error/404/page.tsx
@@ -13,7 +13,7 @@ function Error404() {
       <Navbar />
       <div className="w-screen h-[70vh] flex flex-col justify-center items-center">
         <Image src="/img/error/404_2.svg" alt="" width={400} height={400} />
-        <div className="border border-red-400 p-3 bg-red-100 rounded-md text-center text-lg font-semibold">
+        <div className="min-w-[20%] border border-red-400 p-3 bg-red-100 rounded-md text-center text-lg font-semibold">
           <p>Opps! there is an error!</p>
           <p>Message: {errorMsg ? errorMsg : ""} </p>
         </div>
