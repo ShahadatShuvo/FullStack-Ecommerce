8d2361628ce84b1fa35f68641c5db17d26d9d02c
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-07-10 20:00:25 +0600

    code refactored

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,9 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { useContext } from "react";
+import { GlobalStates } from "@/app/context";
+import Navbar from "@/components/HomePage/Navbar";
 import ContactForm from "@/components/contact/ContactForm";
 
 function Contact() {
+  const { isDarkTheme } = useContext(GlobalStates);
+
   return (
     <div>
-      <ContactForm />
+      <Navbar />
+      <div className={isDarkTheme ? "bg-gray-900" : ""}>
+        <div className="flex min-h-full flex-1 ] flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <span className="w-[200px]">
+              <Image
+                alt="shopping"
+                src="/img/contact.svg"
+                width={200}
+                height={200}
+                priority={true}
+                className="mx-auto w-1/2 md:w-[50vw]"
+              />
+            </span>
+
+            <h2
+              className={
+                isDarkTheme
+                  ? "mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white"
+                  : "mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+              }
+            >
+              Contact with us
+            </h2>
+            <ContactForm />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/contact/ContactForm.tsx
+++ frontend/src/components/contact/ContactForm.tsx
@@ -110,192 +110,152 @@ function ContactForm() {
 
   return (
     <div>
-      <Navbar />
-      <div className={isDarkTheme ? "bg-gray-900" : ""}>
-        <div className="flex min-h-full flex-1 ] flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <span className="w-[200px]">
-              <Image
-                alt="shopping"
-                src="/img/contact.svg"
-                width={200}
-                height={200}
-                priority={true}
-                className="mx-auto w-1/2 md:w-[50vw]"
-              />
-            </span>
-
-            <h2
-              className={
-                isDarkTheme
-                  ? "mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white"
-                  : "mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-              }
-            >
-              Contact with us
-            </h2>
+      <form
+        className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"
+        onSubmit={formik.handleSubmit}
+      >
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-2">
+            <TextField
+              id="outlined-basic"
+              inputProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "black",
+                  background: isDarkTheme ? "#475569" : "white",
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "gray",
+                },
+              }}
+              label="Full Name"
+              variant="outlined"
+              size="small"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              error={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+            />
           </div>
 
-          <form
-            className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"
-            onSubmit={formik.handleSubmit}
+          <div className="flex flex-col gap-2">
+            <TextField
+              id="outlined-basic"
+              inputProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "black",
+                  background: isDarkTheme ? "#475569" : "white",
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "gray",
+                },
+              }}
+              label="Email"
+              variant="outlined"
+              size="small"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <TextField
+              id="outlined-basic"
+              inputProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "black",
+                  background: isDarkTheme ? "#475569" : "white",
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "gray",
+                },
+              }}
+              label="Phone Number"
+              variant="outlined"
+              size="small"
+              name="phone"
+              value={formik.values.phone}
+              onChange={formik.handleChange}
+              error={formik.touched.phone && Boolean(formik.errors.phone)}
+              helperText={formik.touched.phone && formik.errors.phone}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <TextField
+              id="subject"
+              inputProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "black",
+                  background: isDarkTheme ? "#475569" : "white",
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "gray",
+                },
+              }}
+              label="Subject"
+              variant="outlined"
+              size="small"
+              name="subject"
+              value={formik.values.subject}
+              onChange={formik.handleChange}
+              error={formik.touched.subject && Boolean(formik.errors.subject)}
+              helperText={formik.touched.subject && formik.errors.subject}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <TextField
+              id="outlined-multiline-static"
+              sx={{
+                background: isDarkTheme ? "#475569" : "white",
+              }}
+              inputProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "black",
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: isDarkTheme ? "white" : "gray",
+                },
+              }}
+              label="Message"
+              multiline
+              rows={4}
+              className="w-full"
+              name="message"
+              value={formik.values.message}
+              onChange={formik.handleChange}
+              error={formik.touched.message && Boolean(formik.errors.message)}
+              helperText={formik.touched.message && formik.errors.message}
+            />
+          </div>
+
+          <div
+            className={
+              isDarkTheme
+                ? "flex flex-col gap-2 bg-gray-500 text-black rounded-full"
+                : "flex flex-col gap-2 bg-black rounded-full"
+            }
           >
-            <div className="flex flex-col gap-3">
-              <div className="flex flex-col gap-2">
-                <TextField
-                  id="outlined-basic"
-                  inputProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "black",
-                      background: isDarkTheme ? "#475569" : "white",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "gray",
-                    },
-                  }}
-                  label="Full Name"
-                  variant="outlined"
-                  size="small"
-                  name="name"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  error={formik.touched.name && Boolean(formik.errors.name)}
-                  helperText={formik.touched.name && formik.errors.name}
-                />
-              </div>
-
-              <div className="flex flex-col gap-2">
-                <TextField
-                  id="outlined-basic"
-                  inputProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "black",
-                      background: isDarkTheme ? "#475569" : "white",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "gray",
-                    },
-                  }}
-                  label="Email"
-                  variant="outlined"
-                  size="small"
-                  name="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <TextField
-                  id="outlined-basic"
-                  inputProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "black",
-                      background: isDarkTheme ? "#475569" : "white",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "gray",
-                    },
-                  }}
-                  label="Phone Number"
-                  variant="outlined"
-                  size="small"
-                  name="phone"
-                  value={formik.values.phone}
-                  onChange={formik.handleChange}
-                  error={formik.touched.phone && Boolean(formik.errors.phone)}
-                  helperText={formik.touched.phone && formik.errors.phone}
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <TextField
-                  id="subject"
-                  inputProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "black",
-                      background: isDarkTheme ? "#475569" : "white",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "gray",
-                    },
-                  }}
-                  label="Subject"
-                  variant="outlined"
-                  size="small"
-                  name="subject"
-                  value={formik.values.subject}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.subject && Boolean(formik.errors.subject)
-                  }
-                  helperText={formik.touched.subject && formik.errors.subject}
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <TextField
-                  id="outlined-multiline-static"
-                  sx={{
-                    background: isDarkTheme ? "#475569" : "white",
-                  }}
-                  inputProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "black",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      color: isDarkTheme ? "white" : "gray",
-                    },
-                  }}
-                  label="Message"
-                  multiline
-                  rows={4}
-                  className="w-full"
-                  name="message"
-                  value={formik.values.message}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.message && Boolean(formik.errors.message)
-                  }
-                  helperText={formik.touched.message && formik.errors.message}
-                />
-              </div>
-
-              <div
-                className={
-                  isDarkTheme
-                    ? "flex flex-col gap-2 bg-gray-500 text-black rounded-full"
-                    : "flex flex-col gap-2 bg-black rounded-full"
-                }
-              >
-                <Button
-                  variant="contained"
-                  className="rounded-full"
-                  type="submit"
-                >
-                  Submit Message
-                </Button>
-              </div>
-            </div>
-            {alert.msg && (
-              <AuthSuccess
-                msg={alert.msg}
-                type={alert.type}
-                show={alert.show}
-              />
-            )}
-          </form>
+            <Button variant="contained" className="rounded-full" type="submit">
+              Submit Message
+            </Button>
+          </div>
         </div>
-      </div>
+        {alert.msg && (
+          <AuthSuccess msg={alert.msg} type={alert.type} show={alert.show} />
+        )}
+      </form>
     </div>
   );
 }
