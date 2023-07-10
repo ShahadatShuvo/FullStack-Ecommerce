370e7fa83141ba4fccb5779820a3b9db9b7313b9
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-07-10 18:38:22 +0600

    api endpoint created for message display

--- backend/newsroom/models/contact.py
+++ backend/newsroom/models/contact.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 
 class Contact(models.Model):
     name = models.CharField(max_length=200)
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="contacts")
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=20)
     subject = models.CharField(max_length=255)
